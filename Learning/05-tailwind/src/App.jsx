ec11c46e8f021d822b2b42eec3a878c693322ae6
Md Atique Ahmad <atique@gmail.com>
2026-06-08 19:35:47 +0530

    practice exercise to run basics of tailwind

---
+++ Learning/05-tailwind/src/App.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className=" ">
+    <div className="p-6 max-w-sm mx-a bg-perple flex items-center  space-x-2.5">
+      <div ><img className='rounded-4xl' src="https://tse4.mm.bing.net/th/id/OIP.KA9n8HZQypox554UyHOdewHaEJ?pid=Api&P=0&h=180" alt="" />
+      </div>
+      <div className='text-red-500 text-xl'>
+      Tailwind Css 
+      <p> By Atique Ahmad </p>
+    </div></div>
+    
+<button class="bg-violet-500 hover:bg-violet-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-violet-700 ...">
+  Save changes
+</button>
+ 
+ <div className="text-center my-4" >
+  <p className='text-gery sm:text-red-600 md:text-green-600'>Lorem ipsum dolor sit amet.</p>
+ </div>
+
+
+    <div className="mt-3">
+      <div className="max-w-sm mx-auto bg-white rounded-xl overflow-auto md:max-w-2xl"> 
+
+        <div className='md:flex'>
+          <div>
+            <img className='h-48 w-full object-cover md:h-full md :w-48' src="https://images.pexels.com/photos/15736862/pexels-photo-15736862.jpeg" alt="" />
+          </div>
+          <div className='p-8'>
+            <div className="uppercase font-semibold tracking-wide md:uppercase sm:lowercase">An awesome card</div>
+
+            <a className='block mt-1 text' href=""></a>
+
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+
+      
+
+    </div>
+    
+  )
+}
+
+export default App
