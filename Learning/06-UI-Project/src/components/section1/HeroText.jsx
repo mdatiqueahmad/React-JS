8ef8ad1fb9a34e5b5a383cc6b6e54fb7078101fc
Md Atique Ahmad <atique@gmail.com>
2026-06-11 10:58:36 +0530

    UI-Project to boost tailwind css skills

---
+++ Learning/06-UI-Project/src/components/section1/HeroText.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const HeroText = () => {
+  return (
+    <div className='p-6'>
+      <h3 className='mb-7  text-7xl font-bold'>Prospective <br /><span className='' >customer</span>  <br />Segentation</h3>
+    <p className='text-xl w-[90%] font-medium text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum velit ipsam a accusamus. Nesciunt exercitationem perspiciatis inventore beatae culpa incidunt?</p>
+    </div>
+  )
+}
+
+export default HeroText
