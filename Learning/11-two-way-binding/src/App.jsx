e9312c850bf9a3ace49bc2b7223239edcf26a52e
Md Atique Ahmad <atique@gmail.com>
2026-06-15 13:10:11 +0530

    learning two way binding

---
+++ Learning/11-two-way-binding/src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  
+  const [title, settitle] = useState("")
+    let submitHandler = (e)=>{
+     e.preventDefault()
+      console.log("form submitted by" , title)
+      settitle("")
+    }
+
+  return (
+  <div>
+      
+      <form onSubmit={(e) => {
+        submitHandler(e)
+      }}>
+        <input type="text" placeholder="Enter your name" 
+        value={title} 
+        onChange={(e) => {
+          settitle(e.target.value)
+
+        }}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
